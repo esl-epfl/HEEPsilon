7b3603431d122891c71d2c4ef906a7b52d0a9b06
Juan Sapriza <juan.sapriza@epfl.ch>
2023-04-04 17:51:14 +0200

    Can run bitcount. Removed unnecessary printfs

--- sw/applications/bitcount/main.c
+++ sw/applications/bitcount/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #include "csr.h"
 #include "hart.h"
@@ -110,9 +111,25 @@ void cgra_print_perf(void){
     }
 }
 
+unsigned int pseudoRand()
+{
+   static unsigned int z1 = 12345, z2 = 12345, z3 = 12345, z4 = 12345;
+   unsigned int b;
+   b  = ((z1 << 6) ^ z1) >> 13;
+   z1 = ((z1 & 4294967294U) << 18) ^ b;
+   b  = ((z2 << 2) ^ z2) >> 27; 
+   z2 = ((z2 & 4294967288U) << 2) ^ b;
+   b  = ((z3 << 13) ^ z3) >> 21;
+   z3 = ((z3 & 4294967280U) << 7) ^ b;
+   b  = ((z4 << 3) ^ z4) >> 12;
+   z4 = ((z4 & 4294967168U) << 13) ^ b;
+   return (z1 ^ z2 ^ z3 ^ z4);
+}
+
 int main(void) {
 
-    //srand(RNG_SEED);
+    printf("Bitcount!\n");
+    
     plic_interrupt_init(CGRA_INTR);
 
     cgra_config();
@@ -122,8 +139,7 @@ int main(void) {
     for(int r=0 ; r < REPETITIONS ; r++){
 
         /* BEGIN RNG INPUT */
-        unsigned x = 8;
-        //unsigned x = rand() % (0x7fffffff * 2U + 1U); //UINT max
+        unsigned x = pseudoRand() % UINT_MAX;
         cgra_input[3][0] = x;
         /* END RNG INPUT */
 
