d389f57522c79aadb7fe2fee116038fe0ab0276a
Benoît Denkinger <benoit.denkinger@epfl.ch>
2023-07-27 17:22:35 +0200

    typos corrected in CGRA code.

--- sw/applications/cgra_dbl_search/cgra_bitstream.h
+++ sw/applications/cgra_dbl_search/cgra_bitstream.h
@@ -11,7 +11,7 @@
 // #define MAX_PEAK_KER_ID     3 // DEPRECATED
 // #define MIN_MAX_CIRC_KER_ID 4 // DEPRECATED
 
-uint32_t cgra_kem_bitstream[CGRA_KMEM_SIZE] = {
+uint32_t cgra_kmem_bitstream[CGRA_KMEM_SIZE] = {
   0x0,
   0x100d,
   0x11cd,
@@ -31,7 +31,7 @@ uint32_t cgra_kem_bitstream[CGRA_KMEM_SIZE] = {
 };
 
 
-uint32_t cgra_imem_bistream[CGRA_IMEM_SIZE] = {
+uint32_t cgra_imem_bitstream[CGRA_IMEM_SIZE] = {
   0x0,
   0x0,
   0x0,
