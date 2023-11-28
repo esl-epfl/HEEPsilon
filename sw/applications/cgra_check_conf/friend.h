3515ed1e28ea429e3aed101bcaab24c7ce7a25cd
Benoît Denkinger <benoit.denkinger@epfl.ch>
2023-11-28 12:02:31 +0100

    Removed dummy header file not needed anymore with new xheep flow.

--- sw/applications/cgra_check_conf/friend.h
+++
@@ -1,4 +0,0 @@
-
-// No no no Sir, this header file is not here to fix a bug with the cmake compilation flow of x-heep.
-// The main.c file didn't like to be alone, therefore I gave him a friend to play with.
-// Never ever try to remove it are you shall encounter forces beyond your power.
